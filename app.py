c12877305d58d6cc56e8f59f39b956b5c56a1769
Seydaa01 <127429059+seydaoksuzoglu@users.noreply.github.com>
2024-10-04 22:41:13 +0300

    app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def process_images():
     image_1_path = "images-20241002T205543Z-001/images/akhand_b43_325_jpg.rf.583eea641ed8fb6893d946b671c2348b.jpg"
     image_1 = cv2.imread(image_1_path)
 
-    cv2.imwrite('static-20241002T210124Z-001/static/image_1.jpg', image_1)
+    cv2.imwrite('static/image_1.jpg', image_1)
 
     image_2_path = "images-20241002T205543Z-001/images/akhand_b43_345_jpg.rf.21fb0de29feb94e89adfb699801ab1d8.jpg"
     image_2 = cv2.imread(image_2_path)
